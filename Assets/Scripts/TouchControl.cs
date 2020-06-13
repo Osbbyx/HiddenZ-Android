5da79cc7f9b268d402fd70c2b05337a0b075b251
Osbbyx <oliversbbrc@gmail.com>
2020-06-13 19:29:25 -0400

    Todo listo-V1

---
+++ Assets/Scripts/TouchControl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchControl : MonoBehaviour
+{
+    public static int playerTurnAxisTouch = 0;
+    public static string shif;
+    public static int salto = 0;
+
+    void Start()
+    {
+        playerTurnAxisTouch = 0;
+        shif = "a";
+        salto = 0;
+    }
+
+    public void playerLeftUIPointerDown()
+    {
+        playerTurnAxisTouch = -1;
+    }
+
+    public void playerLeftUIPointerUp()
+    {
+        playerTurnAxisTouch = 0;
+    }
+
+    public void playerLeftUIPointerDownR()
+    {
+        playerTurnAxisTouch = 1;
+    }
+
+    public void playerLeftUIPointerUpR()
+    {
+        playerTurnAxisTouch = 0;
+    }
+
+    public void shiftDown()
+    {
+        shif = "s";
+    }
+
+    public void shiftUp()
+    {
+        shif = "a";
+    }
+
+    public void saltoDown()
+    {
+        salto = -1;
+    }
+
+    public void saltoUp()
+    {
+        salto = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
